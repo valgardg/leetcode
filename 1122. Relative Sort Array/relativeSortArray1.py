1fbe9f255e4f08945750e1db96f406f99d568d08
Valgard Oddsson <voddsson@fcmd.local>
2024-06-11 12:37:03 -0400

    completed: solution for 1122

--- 1122. Relative Sort Array/relativeSortArray1.py
+++ 1122. Relative Sort Array/relativeSortArray1.py
@@ -20,3 +20,29 @@ Constraints:
 0 <= arr1[i], arr2[i] <= 1000
 All the elements of arr2 are distinct.
 Each arr2[i] is in arr1.'''
+class Solution(object):
+    def relativeSortArray(self, arr1, arr2):
+        output = []
+        oddList = []
+        
+        # first handle the relative ordering of items that appear in second array
+        for el1 in arr2:
+            for el2 in arr1:
+                if(el2 == el1):
+                    output.append(el2)
+        # handle all elements not appearing in second array
+        for item in arr1:
+            if(item not in arr2):
+                oddList.append(item)
+        oddList.sort()
+        return output + oddList
+    
+def main():
+    sol = Solution().relativeSortArray([2,21,43,38,0,42,33,7,24,13,12,27,12,24,5,23,29,48,30,31],[2,42,38,0,43,21])
+    # expected: 
+    # [2,42,38,0,43,21,5,7,12,12,13,23,24,24,27,29,30,31,33,48]
+    
+    print(sol)
+
+if __name__ == "__main__":
+    main()
