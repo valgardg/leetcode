b4d60299bf74a0d30782d751ca4760949e89aee0
valgardg <valgardo20@ru.is>
2024-09-17 17:18:13 -0400

    progress on suffix, negative values behave incorrectly

--- 238. Product of Array Excep Self/solution.py
+++ 238. Product of Array Excep Self/solution.py
@@ -5,12 +5,16 @@ class Solution:
         pref = [1] * len(nums)
         for i in range(2, len(nums)):
             pref[i] = pref[i-1] * nums[i-1]
-            print(f'i: {i}, pref[i-2]: {pref[i-1]}, nums[i-1]: {nums[i-1]}')
+            # print(f'i: {i}, pref[i-2]: {pref[i-1]}, nums[i-1]: {nums[i-1]}')
         print(f'prefix: {pref}')
         return pref
 
     def suffixArray(self, nums: List[int]) -> List[int]:
-        pass
+        suf = [1] * len(nums)
+        for i in range(len(nums)-2, -1, -1):
+            suf[i] = suf[i+1] * nums[i+1]
+        print(f'suffix: {suf}')
+        return suf
     
     def productExceptSelf(self, nums: List[int]) -> List[int]:
         pref = self.prefixArray(nums)
@@ -24,7 +28,7 @@ class Solution:
                 
 
 solution = Solution()
-print(solution.prefixArray([1,2,3,4]))
+# print(solution.prefixArray([1,2,3,4]))
 # print(solution.suffixArray([1,2,3,4]))
-# print(solution.productExceptSelf([1,2,3,4]))
-# print(solution.prefixArray([-1,1,0,-3,3]))
+print(solution.productExceptSelf([1,2,3,4]))
+print(solution.productExceptSelf([-1,1,0,-3,3]))
