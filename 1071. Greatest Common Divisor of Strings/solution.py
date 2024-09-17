f50ac6a9af55b740c3db5bdbbc54d2e1f6048a2f
valgardg <valgardo20@ru.is>
2024-09-17 11:18:31 -0400

    added: initial solution attempt

---
+++ 1071. Greatest Common Divisor of Strings/solution.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def gcdOfStrings(self, str1, str2):
+        """
+        :type str1: str
+        :type str2: str
+        :rtype: str
+        """
+        l1 = len(str1)
+        topAnswer = ''
+        testChar = ''
+        for char in str2:
+            testChar += char
+            if((str1 == (testChar * (l1 // len(testChar))))):
+                topAnswer = testChar
+        return topAnswer
+
+    def solve(self):
+        print(self.gcdOfStrings("ABABAB", "ABAB"))
+
+solution = Solution()
+solution.solve()
