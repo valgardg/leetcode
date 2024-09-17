3a79b30be5e98482a90a92ba87e248bb67e3cb7a
valgardg <valgardo20@ru.is>
2024-09-17 12:19:48 -0400

    final solution to 1431

--- 1431. Kids With the Greatest Number of Candies/solution.py
+++ 1431. Kids With the Greatest Number of Candies/solution.py
@@ -4,7 +4,8 @@ from typing import List
 class Solution:
     def kidsWithCandies(self, candies: List[int], extraCandies: int) -> List[bool]:
         # 0th kid
-        return ([candy + extraCandies >= max(candies) for candy in candies])
+        maxCandies = max(candies)
+        return ([candy + extraCandies >= maxCandies for candy in candies])
 
 solution = Solution()
 print(solution.kidsWithCandies([2,3,5,1,3], 3))
