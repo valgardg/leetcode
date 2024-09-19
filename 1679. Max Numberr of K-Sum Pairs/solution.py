a81e2107b14ca205d5bbed3e3f2da83ea1fb0c45
valgardg <valgardo20@ru.is>
2024-09-19 11:44:07 -0400

    solved: 1679

--- 1679. Max Numberr of K-Sum Pairs/solution.py
+++ 1679. Max Numberr of K-Sum Pairs/solution.py
@@ -6,14 +6,20 @@ class Solution:
         for i in range(len(nums)):
             needed = k-nums[i]
             if needed in h:
-                # print(f'{needed}(index {h[needed]}) and {nums[i]} summed would equal {k}')
-                del h[needed]
+                
+                if(len(h[needed]) > 1):
+                    h[needed].pop()
+                else:
+                    del h[needed]
                 count += 1
             else:
-                h[nums[i]] = i
-        print(f'{count}\n')
+                if nums[i] in h:
+                    h[nums[i]].append(i)
+                else:
+                    h[nums[i]] = [i]
         return count
 
-Solution().maxOperations([1,2,3,4], 5)
-Solution().maxOperations([3,1,3,4,3], 6)
-# Solution().maxOperations([2,5,4,4,1,3,4,4,1,4,4,1,2,1,2,2,3,2,4,2], 3)
+# Solution().maxOperations([1,2,3,4], 5)
+# Solution().maxOperations([3,1,3,4,3], 6)
+Solution().maxOperations([2,5,4,4,1,3,4,4,1,4,4,1,2,1,2,2,3,2,4,2], 3)
+                        #   x       x       ?     x x x x 
