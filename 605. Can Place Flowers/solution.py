92ff2ceef321470bbaa2b0f87cdae9f72d669469
valgardg <valgardo20@ru.is>
2024-09-20 15:19:18 -0400

    updated: solution to 605

--- 605. Can Place Flowers/solution.py
+++ 605. Can Place Flowers/solution.py
@@ -2,31 +2,27 @@ from typing import List
 
 class Solution:
     def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
-        canPlace = False
-        planted = 0
-        if(len(flowerbed) == 1):
-            if(n > 1):
-                return False
-            if(n == 0):
+        count = 0
+        if len(flowerbed) == 1:
+            if n == 0:
                 return True
-            return not (flowerbed[0])
+            if n == 1 and flowerbed[0] == 0:
+                return True
+            return False
         for i in range(len(flowerbed)):
-            if(i > 0 and i<len(flowerbed)-1):
+            if i > 0 and i != len(flowerbed)-1:
                 if flowerbed[i-1] != 1 and flowerbed[i] != 1 and flowerbed[i+1] != 1:
+                    count+=1
                     flowerbed[i] = 1
-                    planted += 1
             elif i == 0:
                 if flowerbed[i] != 1 and flowerbed[i+1] != 1:
+                    count+=1
                     flowerbed[i] = 1
-                    planted +=1
             elif i == len(flowerbed)-1:
-                if flowerbed[i-1] != 1 and flowerbed[i] != 1:
+                if flowerbed[i] != 1 and flowerbed[i-1] != 1:
+                    count+=1
                     flowerbed[i] = 1
-                    planted +=1
-        return (planted >= n)
-                
+        return count >= n
 
-solution = Solution()
-print(solution.canPlaceFlowers([1,0,0,0,1,0,0], 2))
-print(solution.canPlaceFlowers([0], 1))
-print(solution.canPlaceFlowers([1], 0))
+print(Solution().canPlaceFlowers([0,0,1,0,0], 1))
+print(Solution().canPlaceFlowers([1,0,0,0,1], 2))
