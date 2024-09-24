5b38ee875540830a72a117eb58356036bd50a4d1
valgardg <valgardo20@ru.is>
2024-09-24 13:38:34 -0400

    solved 735

--- 735. Asteroid Collision/solution.py
+++ 735. Asteroid Collision/solution.py
@@ -2,49 +2,43 @@ from typing import List
 
 class Solution:
     def asteroidCollision(self, asteroids: List[int]) -> List[int]:
-        final = []
-
+        n = len(asteroids)
         pp = 0
-        print(f'------ {pp} -------')
 
-        while pp != len(asteroids):
+        while pp != n:
             if asteroids[pp] < 0:
-                if (len(final) < 1):
-                    print(f'appending pointer: {asteroids[pp]}')
-                    final.append(asteroids[pp])
+                if pp == 0:
+                    pp += 1
+                    pass
                 else:
-                    pv = final.pop() # previous value
-                    print(f'pv popped: {pv}')
-                    print(final)
+                    pv = asteroids[pp-1]
                     if(pv < 0):
-                        print(f'appending pv and pointer: {pv} and {asteroids[pp]}')
-                        final.append(pv)
-                        final.append(asteroids[pp])
+                        pp += 1
                     # collision
                     else:
-                        print(f'collision between: {pv} and {asteroids[pp]}')
                         if abs(pv) > abs(asteroids[pp]):
-                            print(f'{pv} (pv) appended')
-                            final.append(pv)
+                            asteroids = asteroids[:pp] + asteroids[pp+1:]
+                            n -= 1
+                            pp -= 1
                         elif abs(pv) == abs(asteroids[pp]):
-                            print(f'destroyed')
-                            pass
+                            asteroids = asteroids[:pp-1] + asteroids[pp+1:]
+                            n -= 2
+                            pp -= 2
                         else:
-                            final.append(asteroids[pp])
-                            print(f'{asteroids[pp]} asteroids[pp] appended')
-
+                            asteroids = asteroids[:pp-1] + asteroids[pp:]
+                            n -= 1
+                            pp -= 1
             else:
-                print(f'appending pointer: {asteroids[pp]}')
-                final.append(asteroids[pp])
+                pp += 1
+            
+            if pp < 0:
+                pp = 0
+            
+        return asteroids
 
-            pp += 1
-            print(final)
-            print(f'------ {pp} -------')
-        return final
-
-# print(Solution().asteroidCollision([5,10,-5]))
-# print(Solution().asteroidCollision([8, -8]))
-# print(Solution().asteroidCollision([10,2,-5]))
-# print(Solution().asteroidCollision([-2,-1,1,2]))
-# print(Solution().asteroidCollision([-2,1,-2,-1]))
+print(Solution().asteroidCollision([5,10,-5]))
+print(Solution().asteroidCollision([8, -8]))
+print(Solution().asteroidCollision([10,2,-5]))
+print(Solution().asteroidCollision([-2,-1,1,2]))
+print(Solution().asteroidCollision([-2,1,-2,-1]))
 print(Solution().asteroidCollision([10,2,-5]))
