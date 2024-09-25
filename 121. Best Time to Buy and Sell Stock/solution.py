ce549e4b170fd4f77ea6efa315855cdc6f7f3ae6
valgardg <valgardo20@ru.is>
2024-09-25 13:41:48 -0400

    solved 121

---
+++ 121. Best Time to Buy and Sell Stock/solution.py
@@ -0,0 +1,22 @@
+from typing import List
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        max_profit = 0
+        
+        lp = 0
+        rp = 0
+
+        while rp < len(prices):
+            profit = prices[rp] - prices[lp]
+            if profit > max_profit:
+                max_profit = profit
+            if prices[rp] < prices[lp]:
+                lp += 1
+            else:
+                rp += 1
+            
+
+        return max_profit
+
+print(Solution().maxProfit([7,1,5,3,6,4]))
+print(Solution().maxProfit([7,6,4,3,1]))
