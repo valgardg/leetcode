b5f3778ef330be66218777b1ae80925ab1be76dc
valgardg <valgardo20@ru.is>
2024-09-28 12:08:32 -0400

    solved: q2 after contest

--- contests/biweekly/140/q2/solutuion.py
+++ contests/biweekly/140/q2/solutuion.py
@@ -5,41 +5,47 @@ class Solution:
         if (max(maximumHeight)) < len(maximumHeight):
             return -1
 
-        lowerCounter = []
-        heights = {}
+        heightl = []
         csum = 0 # current sum
+        heights = {}
 
         for i in range(len(maximumHeight)):
             height = maximumHeight[i]
-            if height in heights:
-                res = self.updateHeight(height, heights, lowerCounter)
-                if res == -1:
-                    return res
-                csum += res
-            else:
-                heights[height] = height
-                if height not in lowerCounter:
-                    lowerCounter.append(height)
+            # print(f'inspecting height {height}, i: {i}, maxheight: {maximumHeight}')
+            if height not in heights:
+                # print(f'height not in heights: {height}')
+                # if we have discovered a new digit, we want to leave as much information in case another one comes along
+                lowerterm = height - 1
+                while lowerterm in heights:
+                    lowerterm = heights[lowerterm]
+                heights[height] = lowerterm
                 csum += height
+                heightl.append(height)
+                # print(f'    set heights[{height}] = {height}')
+                # print(f'    set heights[{height-1}] = {lowerterm}')
+                # print(f'    added {height} to csum {csum}')
+                # print(f'    appended {height} to heightl: {heightl}')
+            else:
+                # print(f'height found in heights: {height}')
+                lowerterm = heights[height]
+                while lowerterm in heights:
+                    lowerterm = heights[lowerterm]
+                csum += lowerterm
+                # print(f'final lowerterm: {lowerterm}')
+                if lowerterm == 0:
+                    return -1
+                heightl.append(lowerterm)
+                heights[height] = lowerterm
+                heights[lowerterm] = lowerterm - 1
+                
 
+        print(f'list at the end: {heightl}')
         return csum
-    
-    def updateHeight(self, height, heights, lowerCounter):
-        # if we are encountering a height for the second time and th
-        existingHeight = height
-        while existingHeight > 1:
-            existingHeight -= 1
-            if existingHeight not in heights:
-                heights[existingHeight] = existingHeight
-                # print(f'updated heights: {heights}')
-                return existingHeight
-        return -1
-            
-
 
 
 print(Solution().maximumTotalSum([2,3,4,3]))
-
+print(Solution().maximumTotalSum([5,5,4,5]))
 print(Solution().maximumTotalSum([15,10]))
-
 print(Solution().maximumTotalSum([2,2,1]))
+print(Solution().maximumTotalSum([1, 10, 5, 1]))
+print(Solution().maximumTotalSum([7,4,5,4,7,2,6]))
