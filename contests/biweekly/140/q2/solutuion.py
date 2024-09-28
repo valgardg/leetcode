58d2d3a94c8b27a0cc858a64161643e8a6bb197b
valgardg <valgardo20@ru.is>
2024-09-28 11:27:50 -0400

    further progress, trying to reduce time complexity

--- contests/biweekly/140/q2/solutuion.py
+++ contests/biweekly/140/q2/solutuion.py
@@ -2,30 +2,38 @@ from typing import List
 class Solution:
     def maximumTotalSum(self, maximumHeight: List[int]) -> int:
         
+        if (max(maximumHeight)) < len(maximumHeight):
+            return -1
+
+        lowerCounter = []
         heights = {}
+        csum = 0 # current sum
 
         for i in range(len(maximumHeight)):
             height = maximumHeight[i]
             if height in heights:
-                res = self.updateHeight(height, heights)
+                res = self.updateHeight(height, heights, lowerCounter)
                 if res == -1:
                     return res
+                csum += res
             else:
                 heights[height] = height
+                if height not in lowerCounter:
+                    lowerCounter.append(height)
+                csum += height
 
-        return sum(heights.values())
+        return csum
     
-    def updateHeight(self, height, heights):
+    def updateHeight(self, height, heights, lowerCounter):
         # if we are encountering a height for the second time and th
-        if height in heights:
-            existingHeight = heights[height]
-            while existingHeight > 1:
-                existingHeight -= 1
-                if existingHeight not in heights:
-                    heights[existingHeight] = existingHeight
-                    # print(f'updated heights: {heights}')
-                    return 1
-            return -1
+        existingHeight = height
+        while existingHeight > 1:
+            existingHeight -= 1
+            if existingHeight not in heights:
+                heights[existingHeight] = existingHeight
+                # print(f'updated heights: {heights}')
+                return existingHeight
+        return -1
             
 
 
