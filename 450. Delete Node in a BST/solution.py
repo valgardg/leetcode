e8333f89972e12633c88a8b0fb14226d87efbb2d
valgardg <valgardo20@ru.is>
2024-10-08 12:55:48 -0400

    solved 450

--- 450. Delete Node in a BST/solution.py
+++ 450. Delete Node in a BST/solution.py
@@ -1,3 +1,5 @@
+from collections import deque
+from utils.treeToList import treeToList
 from utils.convertToTree import listToTreeNode
 from typing import Optional
 
@@ -9,39 +11,45 @@ class TreeNode:
         self.right = right
 class Solution:
     def deleteNode(self, root: Optional[TreeNode], key: int) -> Optional[TreeNode]:
-        if root.val == key:
+        if not root:
             return None
-        def searchBST(root, parent):
-            if not root:
-                return
-            print(f'looking at node: {root.val}')
-            if root.val > key:
-                return searchBST(root.left, root)
-            if root.val < key:
-                return searchBST(root.right, root)
-            # here we knwo t
-            lnode = root.left
-            rnode = root.right
-            if root.val > parent.val:
-                parent.right = rnode
-                rnodeleft = rnode
-                while rnodeleft.left:
-                    rnodeleft = rnodeleft.left
-                rnodeleft.left = lnode
-            if root.val < parent.val:
-                parent.left = rnode
-                rnodeleft = rnode
-                while rnodeleft.left:
-                    rnodeleft = rnodeleft.left
-                rnodeleft.left = lnode
-            parent.left = None
-            parent.right = None
-
-        searchBST(root, root)
-
+        if root.val > key:
+            root.left = self.deleteNode(root.left, key)
+        elif root.val < key:
+            print(f'looking at {root.val}, going into {root.right.val}')
+            root.right = self.deleteNode(root.right, key)
+        # found our key
+        else:
+            print(f'found key: {root.val}')
+            # no children
+            if not root.left and not root.right:
+                print(f'returning none womp womp')
+                return None
+            # no right
+            if root.left and not root.right:
+                return root.left
+            # no left
+            if not root.left and root.right:
+                return root.right
+            # two children
+            if root.left and root.right:
+                minval = self.findmin(root.right)
+                root.val = minval
+                root.right = self.deleteNode(root.right, root.val)
         return root
 
-# root = listToTreeNode([5,3,6,2,4,None,3])
-# print(Solution().deleteNode(root, 3))
-root = listToTreeNode([0])
-print(Solution().deleteNode(root, 0).val)
+    def findmin(self, root):
+        node = root
+        while node.left:
+            node = node.left
+        nodeval = node.val
+        node = None
+        return nodeval
+
+root = listToTreeNode([5,3,6,2,4,None,7])
+sol = Solution().deleteNode(root, 3)
+print(treeToList(sol))
+
+# root = listToTreeNode([5,3,6,2,4,None,7])
+# sol = Solution().deleteNode(root, 7)
+# print(treeToList(sol))
