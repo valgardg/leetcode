a8e49ccaeab250b70336a4934c516b9d084b579c
valgardg <valgardo20@ru.is>
2024-10-08 12:56:21 -0400

    updated utils to include treeToList function

---
+++ utils/treeToList.py
@@ -0,0 +1,24 @@
+from collections import deque
+
+
+def treeToList(root):
+    if not root:
+        return []
+
+    result = []
+    queue = deque([root])
+
+    while queue:
+        node = queue.popleft()
+        if node:
+            result.append(node.val)
+            queue.append(node.left)
+            queue.append(node.right)
+        else:
+            result.append(None)
+
+    # Remove trailing None values that represent non-existent nodes
+    while result and result[-1] is None:
+        result.pop()
+
+    return result
