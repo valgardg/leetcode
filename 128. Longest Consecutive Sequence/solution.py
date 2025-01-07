ce82b1553b8baa7c50f860e192d9ad2cc9eafab4
valgardg <valgardo20@ru.is>
2025-01-06 20:35:54 -0500

    solved 128

--- 128. Longest Consecutive Sequence/solution.py
+++ 128. Longest Consecutive Sequence/solution.py
@@ -1,29 +1,25 @@
 from typing import List
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
-        ht = {}
-        for i in nums:
-            if i not in ht:
-                ht[i] = i
-        # print(ht)
-        longest = 0
-        for i in ht.keys():
-            # print(f'Checking {i}')
-            cc = 1
-            p = ht[i]
-            while p-1 in ht:
-                # print(f'Tunneling {p-1}')
-                cc += 1
-                p = ht[p-1]
-            # print(f'cc: {cc}')
-            if cc > longest:
-                longest = cc
-
+        nums = set(nums)
+        longest = 1 if len(nums) > 0 else 0
+        for num in nums:
+            if num - 1 in nums:
+                continue
+            if num + 1 in nums:
+                nlongest = 1
+                cc = num
+                while cc + 1 in nums:
+                    nlongest += 1
+                    if nlongest > longest:
+                        longest = nlongest
+                    cc +=  1
         return longest
+            
 
 print(Solution().longestConsecutive([100, 4, 200, 1, 3, 2])) # 4
-print(Solution().longestConsecutive([0,3,7,2,5,8,4,6,0,1])) # 4
-print(Solution().longestConsecutive([1,2,0,1])) # 4
-print(Solution().longestConsecutive([0])) # 4
-print(Solution().longestConsecutive([0,0])) # 4
-print(Solution().longestConsecutive([])) # 4
+print(Solution().longestConsecutive([0,3,7,2,5,8,4,6,0,1])) # 9
+print(Solution().longestConsecutive([1,2,0,1])) # 3
+print(Solution().longestConsecutive([0])) # 1
+print(Solution().longestConsecutive([0,0])) # 1
+print(Solution().longestConsecutive([])) # 0
