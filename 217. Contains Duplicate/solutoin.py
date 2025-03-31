f4fddfb1ce7c9362a726bd1f7c044a2ab9054c11
valgardg <70694766+valgardg@users.noreply.github.com>
2025-03-31 10:57:10 +0000

    Update and rename solutoin.py to solution.py
    Updated solution to simple and intuitive one liner

--- 217. Contains Duplicate/solutoin.py
+++
@@ -1,9 +0,0 @@
-from typing import List
-class Solution:
-    def containsDuplicate(self, nums: List[int]) -> bool:
-        vals = {}
-        for num in nums:
-            if num in vals:
-                return True
-            vals[num] = num
-        return False
