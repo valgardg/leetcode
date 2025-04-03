5c39f4a7058a0347dfb24ef0f0027961d12aa1ed
valgardg <valgardo20@ru.is>
2025-04-03 12:09:37 +0000

    solved: 739. Daily Temperatures

--- 739. Daily Temperatures/solution.py
+++ 739. Daily Temperatures/solution.py
@@ -1,16 +1,33 @@
+from collections import defaultdict
 from typing import List
-
-
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
-        temps = []
-        for i in range(temperatures):
+        stack = []
+        positions = defaultdict(list)
+        final = {}
+        for i in range(len(temperatures)):
             temp = temperatures[i]
-            count = 0
-            ii = i + 1
-            while ii < len(temperatures) and temp < temperatures[ii]:
-                count += 1
-                ii += 1
+            positions[temp].append(i)
             
+            while len(stack) > 0 and temp > stack[-1]:
+                popped = stack.pop()
+                poppeed_position = positions[popped].pop()
+                final[poppeed_position] = i - poppeed_position
 
-print(Solution().dailyTemperatures([73,74,75,71,69,72,76,73]))
+            stack.append(temp)
+
+        # print(stack)
+        # print(positions)
+        # print(final)
+
+        for n in stack:
+            while len(positions[n]) > 0:
+                position = positions[n].pop()
+                final[position] = 0
+
+        return list(dict(sorted(final.items())).values())
+
+
+# print(Solution().dailyTemperatures([73,74,75,71,69,72,76,73]))
+# print(Solution().dailyTemperatures([30,60,90]))
+print(Solution().dailyTemperatures([34,80,80,34,34,80,80,80,80,34]))
