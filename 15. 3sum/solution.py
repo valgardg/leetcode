3dab453724fc17bf108a603f3e86a80d440d4100
valgardg <valgardo20@ru.is>
2025-04-09 22:55:52 +0000

    implemented solution to 3sum but causes TLE

--- 15. 3sum/solution.py
+++ 15. 3sum/solution.py
@@ -2,18 +2,32 @@ from typing import List
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         solutions = []
-        nums = set(nums)
-        for num in nums:
-            tslist = [x for x in nums if x != num]
-            target = -1 * num
-            # 2 sum implementation
-            nd = {}
-            for nmbr in tslist:
-                if target - nmbr in nd:
-                    print(f"target: {target}, num: {num}, nmbr: {nmbr}, nd[target - nmbr]: {nd[target - nmbr]}")
-                    solutions.append([num, nmbr, nd[target - nmbr]])
+
+        for i in range(len(nums)):
+            num = nums[i]
+            twoSumHM = {}
+            target = 0 - num
+            print(f"Num: {num} - Targret: {target}")
+            for j in range(len(nums)):
+                if j == i:
+                    continue
+                tnum = nums[j]
+                other = target - tnum
+                # check if we have the other number
+                if other in twoSumHM.keys():
+                    for other_index in twoSumHM[other]:
+                        entry = [nums[i],nums[j],nums[other_index]]
+                        # entry = [i,j,other_index] 
+                        if sorted(entry) not in solutions: 
+                            solutions.append(sorted(entry))
+                    
+                # add current num and index
+                if tnum in twoSumHM.keys():
+                    twoSumHM[tnum].append(j)
                 else:
-                    nd[nmbr] = nmbr
+                    twoSumHM[tnum] = [j]
+
         return solutions
+                
 
 print(Solution().threeSum([-1,0,1,2,-1,-4]))
