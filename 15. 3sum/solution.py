afc4cba2495975033a605e6096e9550094e0be04
valgardg <valgardo20@ru.is>
2025-04-10 23:51:15 +0000

    solved: 15. 3sum with a lot of help and looking at solution >:(

--- 15. 3sum/solution.py
+++ 15. 3sum/solution.py
@@ -1,33 +1,32 @@
 from typing import List
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        solutions = []
+        nums.sort() # we want to sort our list because that way we know left and right pointers point to lowest and highest possibilities
+        # print(nums)
+        res = []
 
         for i in range(len(nums)):
-            num = nums[i]
-            twoSumHM = {}
-            target = 0 - num
-            print(f"Num: {num} - Targret: {target}")
-            for j in range(len(nums)):
-                if j == i:
-                    continue
-                tnum = nums[j]
-                other = target - tnum
-                # check if we have the other number
-                if other in twoSumHM.keys():
-                    for other_index in twoSumHM[other]:
-                        entry = [nums[i],nums[j],nums[other_index]]
-                        # entry = [i,j,other_index] 
-                        if sorted(entry) not in solutions: 
-                            solutions.append(sorted(entry))
-                    
-                # add current num and index
-                if tnum in twoSumHM.keys():
-                    twoSumHM[tnum].append(j)
-                else:
-                    twoSumHM[tnum] = [j]
-
-        return solutions
+            if i > 0 and nums[i] == nums[i-1]:
+                # print(f"Skipping i: {i}")
+                continue
+            lp = i + 1
+            rp = len(nums) - 1
+            while lp < rp:
+                total = nums[i] + nums[lp] + nums[rp]
+                
+                if total > 0:
+                    rp -= 1
+                elif total < 0:
+                    lp += 1
+                elif total == 0:
+                    res.append([nums[i], nums[lp], nums[rp]])
+                    lp += 1
+                    while lp > 0 and nums[lp] == nums[lp-1] and lp < rp:
+                        lp += 1
+        return res
                 
 
 print(Solution().threeSum([-1,0,1,2,-1,-4]))
+print(Solution().threeSum([0,0,0,0]))
+# print(Solution().threeSum([-2,-3,0,0,-2]))
+print(Solution().threeSum([2,-3,0,-2,-5,-5,-4,1,2,-2,2,0,2,-4,5,5,-10]))
