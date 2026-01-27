5d12bf5f35971837e6acef626d9a25933c0b9e1c
valgardg <70694766+valgardg@users.noreply.github.com>
2026-01-27 16:53:36 +0000

    Update Solution.py
    updated to use running sum instead of recalculating sum every iteration, slight optimization

--- Code Testcase Test Result Test Result 3804. Number of Centered Subarrays/Solution.py
+++ Code Testcase Test Result Test Result 3804. Number of Centered Subarrays/Solution.py
@@ -8,12 +8,13 @@ class Solution(object):
         for i, num in zip(range(len(nums)), nums):
 
             checklist = [num]
+            running_sum = num
 
             for ii, second_num in zip(range(len(nums)), nums[i+1:]):
 
                 checklist.append(second_num)
-                listsum = sum(checklist)
-                if(listsum in checklist):
+                running_sum = running_sum + second_num
+                if(running_sum in checklist):
                     count += 1
 
         return count + len(nums)
