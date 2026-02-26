abe7369f78d4a39e172ea46cc7ac023caab1930c
valgardg <70694766+valgardg@users.noreply.github.com>
2026-02-26 21:19:30 +0000

    solved(trie): 208

---
+++ 208. Implement Trie (Prefix Tree)/Solution.py
@@ -0,0 +1,51 @@
+class TrieNode():
+    def __init__(self):
+        self.children = {}
+        self.isEnd = False
+
+class Trie(object):
+
+    def __init__(self):
+        self.root = TrieNode()
+
+    def insert(self, word):
+        """
+        :type word: str
+        :rtype: None
+        """
+        node = self.root
+        for c in word:
+            if c in node.children:
+                node = node.children[c]
+            else:
+                node.children[c] = TrieNode()
+                node = node.children[c]
+        node.isEnd = True
+        
+
+    def search(self, word):
+        """
+        :type word: str
+        :rtype: bool
+        """
+        node = self.root
+        for c in word:
+            if c not in node.children:
+                return False
+            node = node.children[c]
+
+        return node.isEnd
+        
+
+    def startsWith(self, prefix):
+        """
+        :type prefix: str
+        :rtype: bool
+        """
+        node = self.root
+        for c in prefix:
+            if c not in node.children:
+                return False
+            node = node.children[c]
+
+        return True
